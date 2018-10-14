4b1decc7856036780b3f665ec1e237403c6c5a2b
Xkeeper <xkeeper@gmail.com>
2018-10-14 00:40:05 -0700

    Extract starman segment

--- src/music/music-data.asm
+++ src/music/music-data.asm
@@ -60,120 +60,12 @@ MusicData:
 .include "src/music/segments/ending/ending-6.asm"
 
 
+.include "src/music/segments/star/star.asm"
 
 
 
 
 
-
-
-MusicDataStar:
-MusicDataStar_Square2:
-	.db $98
-	.db $48
-	.db $48
-	.db $48
-	.db $94
-	.db $7E
-	.db $48
-	.db $7E
-	.db $99
-	.db $48
-	.db $98
-	.db $48
-	.db $48
-	.db $46
-	.db $46
-	.db $46
-	.db $94
-	.db $7E
-	.db $46
-	.db $7E
-	.db $99
-	.db $46
-	.db $98
-	.db $46
-	.db $46
-	.db $00
-MusicDataStar_Square1:
-	.db $98
-	.db $3A
-	.db $3A
-	.db $3A
-	.db $94
-	.db $34
-	.db $98
-	.db $3A
-	.db $3A
-	.db $94
-	.db $34
-	.db $3A
-	.db $34
-	.db $98
-	.db $3A
-	.db $38
-	.db $38
-	.db $38
-	.db $94
-	.db $30
-	.db $98
-	.db $38
-	.db $38
-	.db $94
-	.db $30
-	.db $38
-	.db $30
-	.db $98
-	.db $38
-MusicDataStar_Triangle:
-	.db $9A
-	.db $34
-	.db $99
-	.db $42
-	.db $4C
-	.db $98
-	.db $7E
-	.db $42
-	.db $4C
-	.db $9A
-	.db $30
-	.db $99
-	.db $3E
-	.db $48
-	.db $98
-	.db $7E
-	.db $3E
-	.db $48
-IFNDEF PROTOTYPE_MUSIC
-MusicDataStar_DPCM:
-	.db $94
-	.db $01
-	.db $98
-	.db $10
-	.db $94
-	.db $10
-	.db $10
-	.db $10
-	.db $01
-	.db $01
-	.db $00
-ENDIF
-IFDEF PROTOTYPE_MUSIC
-MusicDataStar_Noise:
-	.db $98
-	.db $04
-	.db $92
-	.db $02
-	.db $01
-	.db $02
-	.db $98
-	.db $06
-	.db $92
-	.db $02
-	.db $01
-	.db $02
-	.db $00
-ENDIF
 
 IFNDEF PROTOTYPE_MUSIC
 MusicDataUnderground:
